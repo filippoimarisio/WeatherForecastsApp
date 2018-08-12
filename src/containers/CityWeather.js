e8ed2f3b78042d8063b38d391ac6f8e98e64e70f
filippoimarisio <filippo.imarisio@gmail.com>
2018-08-12 19:28:29 +0200

    renders in the component the weather forecasts for the selected city

--- src/containers/CityWeather.js
+++ src/containers/CityWeather.js
@@ -4,24 +4,23 @@ import 'json-loader'
 import cities from '../constants/city.list.json'
 import InputForm from './components/InputForm'
 import {fetchCityWeather} from '../actions/city'
+import DisplayForecasts from './components/DisplayForecasts'
 
 class CityWeather extends React.PureComponent {
 
 
     findCity = (city) => {
-
         const cityOBJ = cities.cities.filter(c =>{
             return c.name === city.city && c.country === city.countryCode
         })[0]
-
         this.props.fetchCityWeather(cityOBJ)
-    }; 
+    };
 
     render() {
-
         return (
             <div className='CityWeatherContainer'>
                 <InputForm findCity={this.findCity}/>
+                <DisplayForecasts cityForecasts={this.props.cityForecasts}/>
             </div>
         )
     }
