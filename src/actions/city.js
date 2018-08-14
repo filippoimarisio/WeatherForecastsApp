101a8309ff5c001ca378f148159729789cbad7cc
filippoimarisio <filippo.imarisio@gmail.com>
2018-08-14 19:25:11 +0200

    fetches forecasts conditionally with current location

--- src/actions/city.js
+++ src/actions/city.js
@@ -2,16 +2,17 @@ import * as request from "superagent";
 import {filipposKey} from '../constants/APIkey'
  
 export const FETCH_CITY_WEATHER = 'FETCH_CITY_WEATHER'
+export const FETCH_CURRENT_CITY_WEATHER = 'FETCH_CURRENT_CITY_WEATHER'
 
 
-const baseUrl = "http://api.openweathermap.org/data/2.5/forecast?id=";
+const baseUrl = "http://api.openweathermap.org/data/2.5/forecast?";
 
 
 export const fetchCityWeather = (cityOBJ) => dispatch => {
     
     const cityID = cityOBJ.id
     request
-      .get(`${baseUrl}${cityID}&APPID=${filipposKey}`)
+      .get(`${baseUrl}id=${cityID}&APPID=${filipposKey}`)
       .then(response =>
         dispatch({
           type: FETCH_CITY_WEATHER,
@@ -20,6 +21,23 @@ export const fetchCityWeather = (cityOBJ) => dispatch => {
       )
   
       .catch(err => alert(err));
+};
+
+export const fetchCurrentCityWeather = (currentCity) => dispatch => {
+    
+  const cityLat = currentCity.coords.latitude
+  const cityLon = currentCity.coords.longitude
+
+  request
+    .get(`${baseUrl}lat=${cityLat}&lon=${cityLon}&APPID=${filipposKey}`)
+    .then(response =>
+      dispatch({
+        type: FETCH_CURRENT_CITY_WEATHER,
+        payload: response.body
+      })
+    )
+
+    .catch(err => alert(err));
 };
 
 
--- src/containers/CityWeather.js
+++ src/containers/CityWeather.js
@@ -3,27 +3,38 @@ import { connect } from 'react-redux'
 import 'json-loader'
 import cities from '../constants/city.list.json'
 import InputForm from './components/InputForm'
-import {fetchCityWeather} from '../actions/city'
+import {fetchCityWeather,fetchCurrentCityWeather} from '../actions/city'
 import {getLocation} from '../actions/currentLocation'
 
 import DisplayForecasts from './components/DisplayForecasts'
 
 class CityWeather extends React.PureComponent {
 
-  componentWillMount() {
+
+  componentDidMount() {
       this.props.getLocation()
-    }
+  }
 
   findCity = (city) => {
-
     const cityOBJ = cities.cities.filter(c =>{
       return c.name === city.city && c.country === city.countryCode
     })[0]
     this.props.fetchCityWeather(cityOBJ)
   };
 
+  findCurrentCity = (currentCity) => {
+    this.props.fetchCurrentCityWeather(currentCity)  
+  }
+  
 
   render() {
+
+    if(this.props.currentLocation.coords && !this.props.cityForecasts.list) {
+      const location = this.props.currentLocation
+      console.log('in the render function', location)
+      this.findCurrentCity(location)
+    }
+
     return (
       <div className='CityWeatherContainer'>
         <InputForm findCity={this.findCity}/>
@@ -38,5 +49,5 @@ const mapStateToProps = ({ cityForecasts, currentLocation }) => {
     return { cityForecasts, currentLocation }
 }
 
-export default connect(mapStateToProps,{fetchCityWeather, getLocation})(CityWeather)
+export default connect(mapStateToProps,{fetchCityWeather, getLocation, fetchCurrentCityWeather})(CityWeather)
 
